4748c4981f15709699cb3cfb3155e94d833a4288
Alex Ariel Tonato  Gudiño <alexarieltonato@hotmail.com>
2024-03-05 17:39:01 -0500

    Se agregan las acciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://ArielTonato.github.io',
+  base: '/Astro-Cafeteria',
 });
