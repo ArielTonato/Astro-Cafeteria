0069e8806a56a25be41770016c72dab8851b7658
Ariel Tonato <alexarieltonato@hotmail.com>
2026-05-21 19:30:46 -0500

    Update asset paths to remove repository-specific prefixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,4 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://ArielTonato.github.io',
-  base: '/Astro-Cafeteria',
 });
